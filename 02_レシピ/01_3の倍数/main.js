9d579797c514720e00b26174e9690d61ace888fa
yui <takahashiyui.0831@gmail.com>
2022-07-10 11:24:36 +0900

    [カウンター]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,10 +1,11 @@
 const genkiFunction = function (number) {
-  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
+    if (number % 3 === 0) {
+      console.log(number + "!!!!!!!")
     } else {
-      console.log(n)
+      console.log(number)
     }
   }
 }
+
+genkiFunction(24)
---
+++ 02_レシピ/01_カウンター/main.js
@@ -0,0 +1,2 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
